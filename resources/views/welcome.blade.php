6f06ac33594efbf6346535913d5191c73f30dd66
sidiq <sidiqaminnudin@gmail.com>
2019-08-03 03:33:44 +0700

    1.1 auth login

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    FakeGram
                 </div>
 
                 <div class="links">
